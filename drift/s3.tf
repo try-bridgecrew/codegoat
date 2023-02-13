da80312644bda97fd564aba4010acb2120275383
jamcg <johnathanmcguigan@gmail.com>
2023-02-13 12:25:39 +0000

    bucket name

--- drift/s3.tf
+++ drift/s3.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "prisma-cloud-code-security-test-bucket-jm"
+  bucket = "prisma-cloud-code-security-drift-bucket-jm"
 
   tags = {
-    Name        = "My PCCS test bucket"
+    Name        = "My drift test bucket"
     Environment = "Prod"
     yor_trace   = "1aa09c71-8ec4-4b2d-a1d6-6fbf1a28f33e"
   }
