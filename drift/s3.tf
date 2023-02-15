8e7a8accbc22e3a0312cf8617bf9349d92331499
jamcg <johnathanmcguigan@gmail.com>
2023-02-15 11:17:02 +0000

    s3 access logging

--- drift/s3.tf
+++ drift/s3.tf
@@ -7,3 +7,14 @@ resource "aws_s3_bucket" "b" {
     yor_trace   = "1aa09c71-8ec4-4b2d-a1d6-6fbf1a28f33e"
   }
 }
+
+resource "aws_s3_bucket" "b_log_bucket" {
+  bucket = "b-log-bucket"
+}
+
+resource "aws_s3_bucket_logging" "b" {
+  bucket = aws_s3_bucket.b.id
+
+  target_bucket = aws_s3_bucket.b_log_bucket.id
+  target_prefix = "log/"
+}
